49aac89a38d616ccbf32c05484b8ba9b97780875
dwidaningrat <142520284+dwidaningrat@users.noreply.github.com>
2026-01-06 22:22:42 +0000

    Update run_seroba.sh

--- run_seroba.sh
+++ run_seroba.sh
@@ -11,7 +11,7 @@ eval "$(conda shell.bash hook)"
 conda activate /x/.conda/envs/myproject
 
 input='/x/fastq/'
-db='/x/seroba/databases/database/'
+db='/x/seroba/database/'
 output='/x/seroba/output/'
 
 mkdir -p "$output"
